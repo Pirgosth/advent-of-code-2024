cb23402cd1bca657e001fdcbf92240e1175c416a
Pierre Dedrie <pirgosth74@gmail.com>
2024-12-16 16:50:55 +0100

    feat: day sixteen finished

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,5 +1,3 @@
-import sys
-
 def get_position(character: str, map: list[str], w: int, h: int) -> tuple[int, int]:
     for j in range(h):
         for i in range(w):
@@ -8,7 +6,7 @@ def get_position(character: str, map: list[str], w: int, h: int) -> tuple[int, i
 
     return (-1, -1)
 
-def get_next_nodes(node: tuple[int, int], map: list[str], w: int, h: int, done: dict[tuple[int, int]]) -> list[tuple[int, int]]:
+def get_next_nodes(node: tuple[int, int], map: list[str], w: int, h: int) -> list[tuple[int, int]]:
     directions = [
         (1, 0),
         (-1, 0),
@@ -20,7 +18,7 @@ def get_next_nodes(node: tuple[int, int], map: list[str], w: int, h: int, done: 
 
     for direction in directions:
         next_node = node[0] + direction[0], node[1] + direction[1]
-        if next_node not in done and map[next_node[1] * w + next_node[0]] in (".", "E"):
+        if map[next_node[1] * w + next_node[0]] in (".", "E"):
             next_nodes.append(next_node)
 
     return next_nodes
@@ -28,45 +26,71 @@ def get_next_nodes(node: tuple[int, int], map: list[str], w: int, h: int, done: 
 def compute_direction(a: tuple[int, int], b: tuple[int, int]) -> tuple[int, int]:
     return b[0] - a[0], b[1] - a[1]
 
-def get_shortest_path(start: tuple[int, int], end: tuple[int, int], map: list[str], w: int, h: int) -> tuple[list[tuple[int, int]], int]:
+def get_shortest_path(visited: dict[tuple[int, int], tuple[int, dict[tuple[int, int], bool]]], shortest_path_length: int) -> dict[tuple[int, int]]:
+    last_node = None
+    for node, directions in visited.items():
+        for score, _ in directions.values():
+            if score == shortest_path_length - 1:
+                last_node = node
+                break
 
-    def _get_shortest_path(fr: tuple[int, int], to: tuple[int, int], done: dict[tuple[int, int]], from_direction: tuple[int, int]) -> tuple[list[tuple[int, int]], int]:
-        next_nodes = get_next_nodes(fr, map, w, h, done)
-        if len(next_nodes) == 0:
-            return [], -1
+    assert last_node is not None
 
-        for next_node in next_nodes:
-            if next_node == to:
-                return [fr, next_node], 1
+    to_explore = [(last_node, shortest_path_length)]
+    
+    done = {}
 
-        next_done = dict(done)
-        next_done[fr] = True
+    while len(to_explore) > 0:
+        node, score = to_explore.pop()
 
-        next_paths = [_get_shortest_path(node, to, next_done, compute_direction(fr, node)) for node in next_nodes]
-        next_paths = [path for path in next_paths if path[1] > 0]
+        if node is None:
+            continue
 
-        if len(next_paths) == 0:
-            return [], -1
+        done[node] = True
 
-        shortest_path = min(next_paths, key=lambda x: x[1])
-        # print(shortest_path)
+        for next_score, next_nodes in visited[node].values():
+            for next_node in next_nodes.keys():
+                if next_node not in done and next_score < score:
+                    to_explore.append((next_node, next_score))
 
-        turn_malus = 0
+    return done
 
-        if len(shortest_path[0]) > 0:
-            to_direction = compute_direction(fr, shortest_path[0][0])
+def get_shortest_path_length(start: tuple[int, int], end: tuple[int, int], map: list[str], w: int, h: int) -> tuple[list[tuple[int, int]], int]:
+    visited = {}
 
-            if from_direction is not None and from_direction != to_direction:
-                turn_malus = 1000
-        
-        # print(turn_malus)
+    to_visit = [(start, 0, (1, 0), None)]
 
-        return [fr] + shortest_path[0], shortest_path[1] + 1 + turn_malus
+    final_scores = []
 
-    return _get_shortest_path(start, end, {}, (1, 0))
+    while len(to_visit) > 0:
+        (node, score, dir, from_node) = to_visit.pop(0)
+
+        if node == end:
+            final_scores.append(score)
+            continue
+
+        if node not in visited:
+            visited[node] = {}
+
+        if dir in visited[node] and score > visited[node][dir][0]:
+            continue
+
+        visited[node][dir] = (score, {from_node: True})
+
+        for next_node in get_next_nodes(node, map, w, h):
+            next_dir = compute_direction(node, next_node)
+            next_score = score + 1
+            if dir != next_dir:
+                next_score += 1000
+
+            to_visit.append((next_node, next_score, next_dir, node))
+
+    shortest_path_length = min(final_scores)
+
+    return shortest_path_length, get_shortest_path(visited, shortest_path_length)
+
 
 def main():
-    sys.setrecursionlimit(3000)
     with open("input.txt", "r", encoding="utf-8") as input:
         map = []
         w, h = 0, 0
@@ -77,13 +101,35 @@ def main():
             for c in line[:-1]:
                 map.append(c)
 
-        print(map)
-
     start = get_position("S", map, w, h)
     end = get_position("E", map, w, h)
-    print(start, end)
 
-    shortest_path = get_shortest_path(start, end, map, w, h)
-    print(shortest_path)
+    shortest_path_length, path = get_shortest_path_length(start, end, map, w, h)
+
+    print("[Part1] Result is", shortest_path_length)
+    # We need to add 1 for the arrival
+    print("[Part2] Result is", len(path) + 1)
+
+    for node in path.keys():
+        map[node[1] * w + node[0]] = "O"
+
+    img = []
+
+    for c in map:
+        if c == "#":
+            img.append((255, 0, 0))
+        elif c == ".":
+            img.append((0, 0, 0))
+        elif c == "O":
+            img.append((0, 255, 0))
+        else:
+            img.append((255, 255, 255))
+
+    from PIL import Image
+
+    im = Image.new("RGB", (w, h))
+    im.putdata(img)
+    im = im.resize((w * 4, h * 4), resample=Image.BOX)
+    im.save(f"output.png")
 
 main()
